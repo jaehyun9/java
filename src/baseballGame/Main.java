5ae8eb4fb9cf35a98208f79c83e90a0fd1b8ffda
jaehyun <ac012235@gmail.com>
2024-10-25 18:54:27 +0900

    new baseballGame

---
+++ src/baseballGame/Main.java
@@ -0,0 +1,10 @@
+package baseballGame;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.play();
+
+    }
+}
