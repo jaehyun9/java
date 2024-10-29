586389ccb0280062401eefb491713e4697afb117
jaehyun9 <ac012235@gmail.com>
2024-10-29 13:03:31 +0900

    Test1 수정

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -3,7 +3,7 @@ package test;
 public class Test1 {
     public static void main(String[] args) {
 
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i < 12; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
